c79b55482618fffa593ca06a87927b6788f3905b
mojokojoxd <kwadwoneer@gmail.com>
2022-07-11 00:25:43 -0600

    initial commit

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const { createCheckout } = require("./controller");
 const app = express();
 app.use(express.json());
-app.use("cors");
+app.use(cors());
 const PORT = process.env.PORT || 3000;
  
  
